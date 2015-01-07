96110c1ef69c33cda6fb93be55aedac81b65772f
Radu Busuioc <ra.busuioc@gmail.com>
2015-01-07 16:26:12 +0000

    added has_more to pagination

--- lib/api_consumer/elements/pagination.rb
+++ lib/api_consumer/elements/pagination.rb
@@ -1,6 +1,6 @@
 module ApiConsumer
   module Elements
-    Pagination = Struct.new(:page, :limit) do
+    Pagination = Struct.new(:page, :limit, :has_more) do
 
       def initialize(attributes)
         attributes.each { |k, v| send("#{k}=", v) if respond_to?("#{k}=") }
